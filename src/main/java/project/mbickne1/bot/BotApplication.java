822bc3448ccc713e8a0eac192c99610084d7c9e2
Matthew Bicknell <64238751+Mbickne1@users.noreply.github.com>
2022-08-22 23:33:04 -0400

    Created Hello World REST endpoint

--- src/main/java/project/mbickne1/bot/BotApplication.java
+++ src/main/java/project/mbickne1/bot/BotApplication.java
@@ -2,12 +2,20 @@ package project.mbickne1.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BotApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World";
 	}
 
 }
