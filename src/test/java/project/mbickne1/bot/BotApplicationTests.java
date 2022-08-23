aaab29eb2725fa866d6af1c7520e799d84bad0fa
Matthew Bicknell <64238751+Mbickne1@users.noreply.github.com>
2022-08-22 21:53:04 -0400

    Initial Commit

---
+++ src/test/java/project/mbickne1/bot/BotApplicationTests.java
@@ -0,0 +1,13 @@
+package project.mbickne1.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
